663add34c5652f3900103906d6b5f4c091ba3d94
Navjot Singh <51900952+Navu4@users.noreply.github.com>
2023-08-27 16:29:53 +0530

    updated img url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,21 +18,21 @@ export default function App({ Component, pageProps }: AppProps) {
                 <link
                     rel="icon"
                     type="image/x-icon"
-                    href="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/262477828-207f86c1-4ac0-4ef9-850e-bacfc6be4641.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230822%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230822T204014Z&X-Amz-Expires=300&X-Amz-Signature=9fa79a5442f271e2f7315d9d240d02667d545fe9bcf5a834b46602ba3bd5d10d&X-Amz-SignedHeaders=host&actor_id=51900952&key_id=0&repo_id=681108642"
+                    href="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/259156872-b43466dd-b48f-4328-8329-7c55343dcfac.png"
                 />
 
                 <meta property="og:type" content="website" />
                 <meta property="og:site_name" content="Navjot" />
                 <meta
                     property="og:image"
-                    content="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/262477828-207f86c1-4ac0-4ef9-850e-bacfc6be4641.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230822%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230822T204014Z&X-Amz-Expires=300&X-Amz-Signature=9fa79a5442f271e2f7315d9d240d02667d545fe9bcf5a834b46602ba3bd5d10d&X-Amz-SignedHeaders=host&actor_id=51900952&key_id=0&repo_id=681108642"
+                    content="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/259156872-b43466dd-b48f-4328-8329-7c55343dcfac.png"
                 />
                 <meta name="twitter:site" content="@Navjot Singh &#128640;" />
                 <meta name="twitter:creator" content="@Navjot Singh &#128640;" />
                 <meta name="twitter:card" content="summary" />
                 <meta
                     name="twitter:image:src"
-                    content="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/262477828-207f86c1-4ac0-4ef9-850e-bacfc6be4641.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230822%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230822T204014Z&X-Amz-Expires=300&X-Amz-Signature=9fa79a5442f271e2f7315d9d240d02667d545fe9bcf5a834b46602ba3bd5d10d&X-Amz-SignedHeaders=host&actor_id=51900952&key_id=0&repo_id=681108642"
+                    content="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/259156872-b43466dd-b48f-4328-8329-7c55343dcfac.png"
                 />
 
                 <base href="/" />
@@ -46,12 +46,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
                 <link
                     rel="apple-touch-icon"
-                    href="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/262477828-207f86c1-4ac0-4ef9-850e-bacfc6be4641.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230822%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230822T204014Z&X-Amz-Expires=300&X-Amz-Signature=9fa79a5442f271e2f7315d9d240d02667d545fe9bcf5a834b46602ba3bd5d10d&X-Amz-SignedHeaders=host&actor_id=51900952&key_id=0&repo_id=681108642"
+                    href="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/259156872-b43466dd-b48f-4328-8329-7c55343dcfac.png"
                     sizes="192x192"
                 />
                 <link
                     rel="alternate"
-                    href="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/262477828-207f86c1-4ac0-4ef9-850e-bacfc6be4641.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230822%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230822T204014Z&X-Amz-Expires=300&X-Amz-Signature=9fa79a5442f271e2f7315d9d240d02667d545fe9bcf5a834b46602ba3bd5d10d&X-Amz-SignedHeaders=host&actor_id=51900952&key_id=0&repo_id=681108642"
+                    href="https://github-production-user-asset-6210df.s3.amazonaws.com/51900952/259156872-b43466dd-b48f-4328-8329-7c55343dcfac.png"
                 />
             </Head>
             <Layout isMobile={isMobile}>
